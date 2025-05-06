81c9b8f0e2cd90fd68dea36fc1fc07c8876066c0
Augusto Teixeira <augusto@impa.br>
2025-05-06 14:04:11 -0300

    fix: fix decoding inside machine

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         .dtb(DTBConfig {
             entrypoint:
                 r#"echo '{"domain":16, "id":"'$(echo -n Hello from inside! | hex --encode)'"}' \
-                     | rollup gio | grep -Eo '0x[0-9a-f]+' | tr -d '\\n' | hex --decode; echo"#
+                     | rollup gio #| grep -Eo '0x[0-9a-f]+' | tr -d '\n' | hex --decode; echo"#
                     .to_string(),
             ..Default::default()
         })
@@ -37,9 +37,9 @@ fn main() {
     machine.run(u64::MAX).unwrap();
     println!("{}",matches!(machine.receive_cmio_request().unwrap(), CmioRequest::Manual(ManualReason::GIO {domain:16, .. })));
     //let a = [0x67, 0x69, 0x6F, 0x2D, 0x72, 0x65, 0x70, 0x6C, 0x79, 0x2D, 0x30];
-    //let a = "0x67696F2D7265706C792D30".as_bytes;
-    //let a = "0x".as_bytes();
-    machine.send_cmio_response(cartesi_machine::types::cmio::CmioResponseReason::Advance, &a).unwrap();
+    let a = "Hi from inside".as_bytes();
+    //let a = "0x00000000".as_bytes();
+    machine.send_cmio_response(cartesi_machine::types::cmio::CmioResponseReason::Inspect, &a).unwrap();
     machine.run(u64::MAX).unwrap();
 }
 
