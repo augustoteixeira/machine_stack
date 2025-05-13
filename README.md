d697fda56fde1cff62c72938e59f481409339da0
Augusto Teixeira <augusto@impa.br>
2025-05-13 14:44:16 -0300

    feat: execute cross-compiled binary

--- README.md
+++ README.md
@@ -2,10 +2,27 @@
 
 Prototype implementation of the Machine Stack
 
-# installation
+# Installation
 
     sudo apt install clang
     wget https://github.com/cartesi/image-kernel/releases/download/v0.20.0/linux-6.5.13-ctsi-1-v0.20.0.bin \
     -O ./linux.bin
     wget https://github.com/cartesi/machine-emulator-tools/releases/download/v0.16.1/rootfs-tools-v0.16.1.ext2 \
     -O ./rootfs.ext2
+
+# Install cross compilation
+
+- install docker and add user to docker group (restart after). Then
+
+    cargo install cross --git https://github.com/cross-rs/cross
+
+- inside one of the app folders (like test-cross), run
+
+    cross build --target riscv64gc-unknown-linux-gnu
+    mkdir exec
+    cp ./test-cross/target/riscv64gc-unknown-linux-gnu/debug/test-cross exec
+
+# assembling the file system
+
+    sudo apt install e2tools
+    e2cp -P 755 ./test-cross/target/riscv64gc-unknown-linux-gnu/debug/test-cross rootfs.ext2:/home/dapp
--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,33 @@ use cartesi_machine::config::machine::{
 
 use cartesi_machine::config::machine::DTBConfig;
 use cartesi_machine::config::runtime::{HTIFRuntimeConfig, RuntimeConfig};
-use cartesi_machine::types::cmio::{CmioRequest, ManualReason};
+use cartesi_machine::types::cmio::{
+    CmioRequest, CmioResponseReason, ManualReason,
+};
 use cartesi_machine::Machine;
 
+const TEST_GIO: &str = r#"echo '{ "domain": 16,
+                          "id": "'$(echo -n Hi from inside! \
+                                     | hex --encode)'"}' \
+                    | rollup gio | grep -Eo '0x[0-9a-f]+' | tr -d '\n' \
+                    | hex --decode; echo
+                 "#;
+
+const TEST_HELLO: &str = r#"rullup"#;
+
 fn main() {
-    let machine_config =         MachineConfig::new_with_ram(RAMConfig {
-            length: 134217728,
-            image_filename: "linux.bin".into(),
-        })
-        .dtb(DTBConfig {
-            entrypoint:
-                r#"echo '{"domain":16, "id":"'$(echo -n Hello from inside! | hex --encode)'"}' \
-                     | rollup gio #| grep -Eo '0x[0-9a-f]+' | tr -d '\n' | hex --decode; echo"#
-                    .to_string(),
-            ..Default::default()
-        })
-        .add_flash_drive(MemoryRangeConfig {
-            image_filename: "rootfs.ext2".into(),
-            ..Default::default()
-        });
+    let machine_config = MachineConfig::new_with_ram(RAMConfig {
+        length: 134217728,
+        image_filename: "linux.bin".into(),
+    })
+    .dtb(DTBConfig {
+        entrypoint: "./test-cross; yield manual rx-accepted".to_string(),
+        ..Default::default()
+    })
+    .add_flash_drive(MemoryRangeConfig {
+        image_filename: "rootfs.ext2".into(),
+        ..Default::default()
+    });
     let runtime_config = RuntimeConfig {
         htif: Some(HTIFRuntimeConfig {
             no_console_putchar: Some(false),
@@ -30,16 +38,20 @@ fn main() {
         ..Default::default()
     };
 
-
     let mut machine =
         Machine::create(&machine_config, &runtime_config).unwrap();
 
     machine.run(u64::MAX).unwrap();
-    println!("{}",matches!(machine.receive_cmio_request().unwrap(), CmioRequest::Manual(ManualReason::GIO {domain:16, .. })));
-    //let a = [0x67, 0x69, 0x6F, 0x2D, 0x72, 0x65, 0x70, 0x6C, 0x79, 0x2D, 0x30];
+    if let CmioRequest::Manual(ManualReason::GIO { domain: 16, data }) =
+        machine.receive_cmio_request().unwrap()
+    {
+        println!("Received gio request for data: {:?}", data);
+    }
+
     let a = "Hi from inside".as_bytes();
-    //let a = "0x00000000".as_bytes();
-    machine.send_cmio_response(cartesi_machine::types::cmio::CmioResponseReason::Inspect, &a).unwrap();
+    machine
+        .send_cmio_response(CmioResponseReason::Inspect, &a)
+        .unwrap();
     machine.run(u64::MAX).unwrap();
 }
 
